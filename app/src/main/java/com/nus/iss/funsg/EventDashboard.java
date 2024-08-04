8dd053f9339ba7f3a1443ae0cb48b7ba13e7b503
howdybunny <e1327869.u.nus.edu>
2024-08-04 22:10:57 +0800

    finish event list

--- app/src/main/java/com/nus/iss/funsg/EventDashboard.java
+++ app/src/main/java/com/nus/iss/funsg/EventDashboard.java
@@ -9,12 +9,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class EventDashboard extends AppCompatActivity {
-
+    private long eventId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_event_dashboard);
-
+        eventId=getIntent().getLongExtra("eventId",0L);
     }
 }
